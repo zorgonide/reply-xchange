f656aa94063aac8c808590d374c1e7992b838ed6
zorgonide <ammar1998@gmail.com>
2024-04-11 14:56:07 +0100

    Add options to FileUploadCard.jsx

--- src/Components/FileUploadCard/FileUploadCard.jsx
+++ src/Components/FileUploadCard/FileUploadCard.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 function FileUploadCard() {
     const [selectedFile, setSelectedFile] = useState(null);
     const [name, setName] = useState('');
+    const [selectedOption, setSelectedOption] = useState('');
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
     };
@@ -12,6 +13,25 @@ function FileUploadCard() {
         const regex = /^[a-zA-Z0-9_]*$/;
         return regex.test(name);
     };
+    const options = [
+        '3d-model',
+        'analog-film',
+        'anime',
+        'cinematic',
+        'comic-book',
+        'digital-art',
+        'enhance',
+        'fantasy-art',
+        'isometric',
+        'line-art',
+        'low-poly',
+        'modeling-compound',
+        'neon-punk',
+        'origami',
+        'photographic',
+        'pixel-art',
+        'tile-texture',
+    ];
     const handleSubmit = (event) => {
         event.preventDefault();
         if (!selectedFile || !name || !checkName(name)) {
@@ -21,6 +41,7 @@ function FileUploadCard() {
         let data = new FormData();
         data.append('fileName', name);
         data.append('filePath', selectedFile);
+        data.append('preset', selectedOption);
 
         let config = {
             method: 'post',
@@ -44,8 +65,8 @@ function FileUploadCard() {
     };
     return (
         <div className='flex flex-col justify-center items-center h-screen'>
-            <div className='bg-gray-200 p-4'>
-                <div className='container mx-auto p-4'>
+            <div className='bg-gray-200 p-4 w-2/6'>
+                <div className='container mx-auto p-4 '>
                     <form onSubmit={handleSubmit} className='flex flex-col items-center'>
                         <label className='block mb-2 text-lg font-large'>Upload your photo</label>
                         <input
@@ -63,6 +84,20 @@ function FileUploadCard() {
                             className='block w-full text-md  bg-gray-50 border border-gray-300  focus:outline-none '
                             onChange={(e) => setName(e.target.value)}
                         />
+                        <div className='flex flex-wrap justify-center gap-1 my-4'>
+                            {options.map((option) => (
+                                <button
+                                    type='button'
+                                    key={option}
+                                    className={`px-4 py-2 rounded-full text-sm font-medium ${
+                                        selectedOption === option ? 'bg-gray-500 text-white' : 'bg-gray-200 text-gray-800'
+                                    } hover:bg-gray-400 hover:text-white transition-colors`}
+                                    onClick={() => setSelectedOption(option)}
+                                >
+                                    {option.replace('-', ' ')}
+                                </button>
+                            ))}
+                        </div>
                         <button
                             type='submit'
                             className='mt-2 px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
