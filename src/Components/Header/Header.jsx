23c4355238ce31aae7f47031bf7a8780c87989be
zorgonide <ammar1998@gmail.com>
2024-04-23 11:02:59 +0100

    Update header background color and text size in Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
-
 function Header() {
     return (
-        <header className='bg-gray-400 text-white px-4 top-0 left-0 right-0 z-50'>
+        <header className='bg-cred  text-lg text-white px-4 top-0 left-0 right-0 z-50'>
             <nav className='flex justify-between items-center'>
                 <ul className='flex justify-start space-x-4'>
                     <li>
