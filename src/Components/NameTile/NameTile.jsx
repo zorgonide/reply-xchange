2abd794862f915dadef76d1588be4039dc82bdc8
zorgonide <ammar1998@gmail.com>
2024-05-03 17:13:49 +0100

    Refactor NameTile component to improve UI and add flex layout

--- src/Components/NameTile/NameTile.jsx
+++ src/Components/NameTile/NameTile.jsx
@@ -5,11 +5,11 @@ function NameTile({ username, routeChange, cat }) {
     return (
         <div
             key={username}
-            className='cursor-pointer bg-white font-semibold  border  shadow-sm text-cred text-center hover:bg-gray-50'
+            className='cursor-pointer bg-white min-h-48 font-semibold  border  shadow-sm text-cred text-center hover:bg-gray-50 flex flex-col'
             onClick={() => routeChange(username)}
         >
-            {cat ? <img src={cat} alt='cat' className='rounded-none h-40' /> : null}
-            <p className='my-2 text-lg'>{username.replace(/[^a-zA-Z]/g, '')}</p>
+            {cat ? <img src={cat} alt='cat' className='rounded-none h-40 ' /> : null}
+            <p className='my-2 text-lg order-last'>{username.replace(/[^a-zA-Z]/g, '')}</p>
         </div>
     );
 }
