b1aa606d1b553939c46293fb1cec18f06e48c970
zorgonide <ammar1998@gmail.com>
2024-05-20 20:33:21 +0100

    feat: Add main text and description to GameSessionsPage

--- src/Pages/GameSessionsPage.jsx
+++ src/Pages/GameSessionsPage.jsx
@@ -37,6 +37,8 @@ function GameSessionsPage() {
     return (
         <div className='min-h-screen bg-cblue'>
             <div className='container p-4 mx-auto'>
+                <h1 className='text-4xl my-4 text-white font-mono font-bold'>Game Sessions</h1>
+                <p className='text-white mb-4'>Select a user to start a game session</p>
                 <div className='flex gap-9'>
                     <div className='grow'>
                         <div className='grid grid-cols-6 gap-4'>
